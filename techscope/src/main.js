251721a283759085acd0188de40a1087e76804dc
ArsenTolkov1305 <formycomputer1305@gmail.com>
2025-01-03 20:25:37 +0200

    update all

---
+++ techscope/src/main.js
@@ -0,0 +1,59 @@
+const sliderControls = document.querySelector(".slider-controls");
+const sliderTabs = sliderControls.querySelectorAll(".slider-tab");
+const sliderIndicator = sliderControls.querySelector(".slider-indicator");
+
+// Update the indicator
+const updateIndicator = (tab, index) => {
+  document.querySelector(".slider-tab.current")?.classList.remove("current");
+  tab.classList.add("current");
+
+  sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
+  sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+
+  // Calculate the scroll position and scroll smoothly
+  const scrollLeft = sliderTabs[index].offsetLeft - sliderControls.offsetWidth / 2 + sliderTabs[index].offsetWidth / 2;
+  sliderControls.scrollTo({ left: scrollLeft, behavior: "smooth" });
+}
+
+// Initialize swiper instance
+const swiper = new Swiper(".slider-container", {
+  effect: "fade",
+  speed: 1300,
+  autoplay: { delay: 4000 },
+  navigation: {
+    prevEl: "#slide-prev",
+    nextEl: "#slide-next",
+  },
+  on: {
+    // Update indicator on slide change
+    slideChange: () => {
+      const currentTabIndex = [...sliderTabs].indexOf(sliderTabs[swiper.activeIndex]);
+      updateIndicator(sliderTabs[swiper.activeIndex], currentTabIndex);
+    },
+    reachEnd: () => swiper.autoplay.stop(),
+  },
+});
+
+// Update the slide on tab click
+sliderTabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    swiper.slideTo(index);
+    updateIndicator(tab, index);
+  });
+});
+
+updateIndicator(sliderTabs[0], 0);
+window.addEventListener("resize", () => updateIndicator(sliderTabs[swiper.activeIndex], 0));
+
+// ScrollReveal
+ScrollReveal().reveal('h1');
+ScrollReveal().reveal('h2');
+ScrollReveal().reveal('h3');
+ScrollReveal().reveal('ul');
+ScrollReveal().reveal('li');
+ScrollReveal().reveal('p');
+ScrollReveal().reveal('h4');
+ScrollReveal().reveal('div');
+ScrollReveal().reveal('img');
+ScrollReveal().reveal('button');
+ScrollReveal().reveal('small');
